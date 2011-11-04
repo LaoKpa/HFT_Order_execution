7c2744e33ac714d1361a75807cd9ebf71e362735
ihar.marozau <ihar.marozau@disfa.com>
2011-11-04 15:54:31 +0300

    add new class OMS

---
+++ OMS.R
@@ -0,0 +1,10 @@
+# TODO: Add comment
+# 
+# Author: Marozau
+###############################################################################
+
+
+setClass("OMS", representation(
+			tick_data = "CTrade_data"
+		),
+)
